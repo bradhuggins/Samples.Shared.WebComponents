ca3ce7875ff615e885d57ea8d10c20c20f36c3d7
Brad Huggins <bhuggins@isn.com>
2021-02-07 15:25:13 -0600

    Initial Commit

---
+++ Shared.WebComponents/Controllers/ComponentsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.WebComponents.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shared.WebComponents.Controllers
+{
+    public class ComponentsController : Controller
+    {
+        public ActionResult HeaderMenu()
+        {
+            HeaderMenuViewModel model = new HeaderMenuViewModel {
+                FirstName = "Test",
+                LastName = "User",
+                LastLoginTimestamp = DateTime.Now.AddHours(-3)
+            };
+            return PartialView(model);
+        }
+
+        public ActionResult Footer()
+        {           
+            return PartialView();
+        }
+
+        public ActionResult BlueBox()
+        {
+            return PartialView();
+        }
+
+    }
+}
---
+++ Shared.WebComponents/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Shared.WebComponents.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
